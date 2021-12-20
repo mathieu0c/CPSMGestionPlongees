48381443ed1c50a1a5c8d7c7e1a7c931827c339a
mathieu0c <10831469-mathieu0c@users.noreply.gitlab.com>
2021-12-20 21:34:29 +0100

    base work

---
+++ CPSMGestionPlongees/global.hpp
@@ -0,0 +1,24 @@
+#ifndef GLOBAL_GLOBAL_HPP
+#define GLOBAL_GLOBAL_HPP
+
+#include <QString>
+
+namespace global
+{
+
+//-- DB
+
+static const QString table_divers{"Divers"};
+static const QString table_diversAddresses{"DiversAddresses"};
+static const QString table_divingSites{"DivingSites"};
+static const QString table_diverLevel{"DiverLevel"};
+static const QString table_dives{"Dives"};
+static const QString table_divesMembers{"DivesMembers"};
+
+//-- format
+
+static const QString format_date{"yyyy-MM-dd"};
+
+}
+
+#endif // GLOBAL_HPP
---
+++ CPSMGestionPlongees/main.cpp
@@ -0,0 +1,17 @@
+#include "GUI/MainWindow.h"
+
+#include <QApplication>
+
+//define global var
+#include "Info/global.cpp"
+#include "GUI/global.cpp"
+#include "Data/global.cpp"
+#include "Debug/global.cpp"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    gui::MainWindow w;
+    w.show();
+    return a.exec();
+}
