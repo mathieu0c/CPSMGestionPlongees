798f2e85b5167ee58503fcf27920ebd8dba66945
mathieu0c <10831469-mathieu0c@users.noreply.gitlab.com>
2021-12-28 22:11:42 +0100

    made it work with CMake

--- CPSMGestionPlongees/main.cpp
+++ CPSMGestionPlongees/main.cpp
@@ -3,10 +3,10 @@
 #include <QApplication>
 
 //define global var
-#include "Info/global.cpp"
-#include "GUI/global.cpp"
-#include "Data/global.cpp"
-#include "Debug/global.cpp"
+//#include "Info/global.cpp"
+//#include "GUI/global.cpp"
+//#include "Data/global.cpp"
+//#include "Debug/global.cpp"
 
 int main(int argc, char *argv[])
 {
