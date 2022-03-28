86352d74ec445cd87edfe501b5f39895b1d6f548
mathieu0c <87909957+mathieu0c@users.noreply.github.com>
2022-03-28 17:42:47 +0300

    Fixed header + update URL

--- CPSMGestionPlongees/Update/PostUpdate.cpp
+++ CPSMGestionPlongees/Update/PostUpdate.cpp
@@ -1,5 +1,5 @@
 /* Broken The Crew 2 sequential clutch assist
- * Copyright (C) 2022 BrokenGameNoob <brokengamenoob@gmail.com>
+ * Copyright (C) 2022 Copyright (C) 2022 BrokenGameNoob <brokengamenoob@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
